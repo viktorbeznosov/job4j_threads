4d1e119f32c70e465cae041f2fe2505d726b9ea7
viktorbeznosov <viktorbeznosov@mail.ru>
2025-05-18 20:05:00 +0300

    2. Модель памяти Java [#267917]

---
+++ src/main/java/ru/job4j/concurrent/DCLSingleton.java
@@ -0,0 +1,21 @@
+package ru.job4j.concurrent;
+
+public final class DCLSingleton {
+
+    private static volatile DCLSingleton instance;
+
+    public static DCLSingleton getInstance() {
+        if (instance == null) {
+            synchronized (DCLSingleton.class) {
+                if (instance == null) {
+                    instance = new DCLSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private DCLSingleton() {
+    }
+
+}
