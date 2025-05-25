a38ba9528f45f5753e2ce9dddf56e06d98a301f9
viktorbeznosov <viktorbeznosov@mail.ru>
2025-05-25 18:50:02 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/io/GetFileContent.java
@@ -0,0 +1,41 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.function.Predicate;
+
+public final class GetFileContent {
+
+    private final File file;
+
+    public GetFileContent(File file) {
+        this.file = file;
+    }
+
+    public synchronized String getContent() throws IOException {
+        try (BufferedReader input = new BufferedReader(new FileReader(file))) {
+            StringBuilder builder = new StringBuilder();
+            input.lines().forEach(data -> builder.append(data));
+            return builder.toString();
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+
+    public synchronized String getContent(Predicate<Character> filter) throws IOException {
+        try (BufferedReader input = new BufferedReader(new FileReader(file))) {
+            String output = "";
+            int data;
+            while ((data = input.read()) > 0) {
+                if (filter.test((char) data)) {
+                    output += (char) data;
+                }
+            }
+            return output;
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/io/SaveFileContent.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public final class SaveFileContent {
+
+    private final File file;
+
+    public SaveFileContent(File file) {
+        this.file = file;
+    }
+
+    public synchronized void saveContent(String content) throws IOException {
+        try (BufferedOutputStream o = new BufferedOutputStream(new FileOutputStream(file))) {
+            for (int i = 0; i < content.length(); i++) {
+                o.write(content.charAt(i));
+            }
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+}
