126c662300689bcf5940be69b4dc9eb5814eb854
viktorbeznosov <viktorbeznosov@mail.ru>
2025-06-04 19:32:41 +0300

    3. Денежные переводы AccountStorage [#1104 #545046]

--- src/main/java/ru/job4j/cash/Account.java
+++ src/main/java/ru/job4j/cash/Account.java
@@ -1,19 +1,4 @@
 package ru.job4j.cash;
 
-public final class Account {
-    private final int id;
-    private final int amount;
-
-    Account(int id, int amount) {
-        this.id = id;
-        this.amount = amount;
-    }
-
-    public int id() {
-        return id;
-    }
-
-    public int amount() {
-        return amount;
-    }
+public record Account(int id, int amount) {
 }
