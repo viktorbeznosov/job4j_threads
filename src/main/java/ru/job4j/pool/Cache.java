544f6cca92ed2adc036e00218510105034ea5ddf
viktorbeznosov <viktorbeznosov@mail.ru>
2025-08-03 16:57:25 +0300

    2. ExecutorService рассылка почты. [#63097 #547632]

--- src/main/java/ru/job4j/pool/Cache.java
+++ src/main/java/ru/job4j/pool/Cache.java
@@ -7,47 +7,25 @@ import java.util.concurrent.TimeUnit;
 public class Cache<T> {
     private Map<String, T> map = new ConcurrentHashMap<>();
 
-    public void put(String key, T value, int timeToLive) {
-        map.put(key, value);
-        removeValueFromTimeToLive(key, timeToLive);
+    public Cache(int timeToLive) {
+        Executors.newFixedThreadPool(1).submit(
+                () -> {
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(timeToLive);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    map.keySet()
+                        .forEach(key -> map.remove(key));
+                }
+        );
     }
 
-    private void removeValueFromTimeToLive(String key, int timeToLive) {
-        Executors.newFixedThreadPool(1).submit(
-            () -> {
-                try {
-                    TimeUnit.MILLISECONDS.sleep(timeToLive);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                map.remove(key);
-            }
-        );
+    public void put(String key, T value) {
+        map.put(key, value);
     }
 
     public T get(String key) {
         return map.get(key);
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        Cache<String> cache = new Cache<>();
-        cache.put("1", "TTL = 10 seconds", 10000);
-        cache.put("2", "TTL = 15 seconds", 15000);
-        cache.put("3", "TTL = 10 seconds", 10000);
-        cache.put("4", "TTL = 10 seconds", 10000);
-        cache.put("5", "TTL = 10 seconds", 10000);
-        cache.put("6", "TTL = 20 seconds", 20000);
-        TimeUnit.MILLISECONDS.sleep(3000);
-        System.out.println(cache.get("1"));
-        System.out.println(cache.get("2"));
-        System.out.println(cache.get("6"));
-        TimeUnit.MILLISECONDS.sleep(8000);
-        System.out.println(cache.get("1"));
-        System.out.println(cache.get("2"));
-        System.out.println(cache.get("6"));
-        TimeUnit.MILLISECONDS.sleep(10000);
-        System.out.println(cache.get("1"));
-        System.out.println(cache.get("2"));
-        System.out.println(cache.get("6"));
     }
 }
